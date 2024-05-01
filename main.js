3d036e791f3daf3bc2666e9434b47c29005fd233
JoeWhoop <147258204+JoeWhoop@users.noreply.github.com>
2024-05-01 19:36:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,12 +7,19 @@ document.addEventListener("DOMContentLoaded", function() {
         var link1 = document.getElementById('link1');
         var link2 = document.getElementById('link2');
 
+        // Dodaj wartość mechanic do linków
+        var selectedMechanic = encodeURIComponent(mechanicValue);
+        var link1URL = "https://web.miniextensions.com/S0nDKlj5o22zpJjfBuAk?prefill_Mechanic=" + selectedMechanic;
+        var link2URL = "https://web.miniextensions.com/p7ZHq803Sx4FebfxAr1M?prefill_Mechanic=" + selectedMechanic;
+
         // Jeśli wybrano QC - A7 w dropdownie dla stacji, użyj linku 1
         if (stationValue === "QC - A7") {
+            link1.href = link1URL;
             link1.click();
         } 
         // Jeśli wybrano QC1 lub QC2 w dropdownie dla stacji, użyj linku 2
         else if (stationValue === "QC1" || stationValue === "QC2") {
+            link2.href = link2URL;
             link2.click();
         }
     });
